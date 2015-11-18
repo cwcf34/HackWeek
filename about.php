ad3842e4217adb169524cca3102d7b1a870f2ded
GregHalligan <gahvg9@mail.missouri.edu>
2015-11-17 22:24:48 -0600

    Changed Words
    PureDay has been changed to OneWord

--- about.php
+++ about.php
@@ -1,1 +1,1 @@
-<p>This project was made by Cody Cameron and Greg Halligan as a project for our Hack Week in our CS4830 course. Wed decided to manipulate the Yelp API to help people find restaurants and activities to do when they are in a new city.</p>
+<p>This project was made by Cody Cameron and Greg Halligan as a project for our Hack Week in our CS4830 course. Wed decided to manipulate the SoundCloud API to help people find songs that have their favorite word.</p>
