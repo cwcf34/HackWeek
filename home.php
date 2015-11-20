bb0914ed07fd7c28aee0803908e64a8079838525
GregHalligan <gahvg9@mail.missouri.edu>
2015-11-20 00:08:26 -0600

    Updated Home.php

--- home.php
+++ home.php
@@ -1,19 +1,39 @@
-<div id="main">
-    <div class="header">
-        <h1>OneWord</h1>
-        <h2>Enter 1 Word</h2>
-    </div>
 
-    <div class="content">
-        <form class="pure-form pure-form-stacked">
-    <fieldset>
+<div id = "main">
+  <div class="header">
+<script>var SELECTION;</script>
+  <h1>OneWord</h1>
+  <h2>Enter 1 Word</h2>
+</div>
+<form method="post">
+<fieldset>
+<label for="word">Word</label>
+<input type="text" name='myText' id="myText" style="background-color: #090067"placeholder="Houdini">
+<div class"content">
+<p>Click the Button to see something awesome happen!</p>
 
-        <label for="word">Word</label>
-        <input id="word" type="word" placeholder="Ex: all">
-
-
-        <button type="submit" class="button-secondary pure-button">Submit</button>
-    </fieldset>
+<button type="Submit" class="button-secondary pure-button">Submit</button>
 </form>
-    </div>
+<script>
+function myFunction() {
+    //document.getElementById("myText").value = "Johnny Bravo";
+    //SELECTION = document.getElementById("myText").value;
+    //window.alert(SELECTION);
+}
+</script>
+
+<?php
+
+  if isset($_POST['myText']) : ?>
+
+<?php
+
+echo $_POST['myText'];
+ ?>
+
+<?php endif?>
+
+
+</fieldset>
+  </div>
 </div>
