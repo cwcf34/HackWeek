4f17bfd5d9d52eb5910b129bc27e9d686737abf8
cwcf34 <cwcf34@mail.missouri.edu>
2015-11-20 20:56:36 -0600

    Styled a few more aspects

--- home.php
+++ home.php
@@ -1,18 +1,17 @@
 <div class="header">
+<script>var SELECTION;</script>
   <h1>OneWord</h1>
   <h2>Enter 1 Word</h2>
 </div>
 </br>
-<div class="content"
+<div class="content">
 <form class="pure-form" action="process.php" method="POST">
 <fieldset>
-  <label for="word">Enter A Word: </label>
+  <label for="word">Enter a Word: </label>
   <input type="text" name='myText' id="myText" placeholder="Houdini">
-
   <p>Click the Button to see something awesome happen!</p>
 
   <button type="submit" class="button-secondary pure-button">Submit</button>
 </fieldset>
 </form>
-
 </div>
--- process.php
+++ process.php
@@ -7,8 +7,6 @@ if (isset($_POST['myText'])) {
   $cookie_value = (htmlspecialchars($_POST['myText']));
   setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 
-
-
  }
 
  header("Location: http://codycameron.me");
