d3d4b75b55cbc443709066ac911010df37f30eb3
GregHalligan <gahvg9@mail.missouri.edu>
2015-11-20 20:46:00 -0600

    Updated The About Me and Contact Us Headers

--- contact.php
+++ contact.php
@@ -1,4 +1,4 @@
 <div class='header'><h1>We'd love to hear back from you!</h1>
-<h3>Our email addresses are<br>
-Greg Halligan: gahvg9@mail.missouri.edu<br>
+<h2>Our email addresses are</h2><br>
+<h3>Greg Halligan: gahvg9@mail.missouri.edu<br>
 Cody Cameron: cwcf34@mail.missouri.edu</h3></div>
